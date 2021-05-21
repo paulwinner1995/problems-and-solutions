16b899d31b4138e952260cde7e6588f826da04a1
Pavel Padalka <ppadalka.pw@gmail.com>
2021-05-21 17:14:05 +0300

    enhance solution for problem 12

--- src/test/java/com/leetcode/Problem12.java
+++ src/test/java/com/leetcode/Problem12.java
@@ -45,14 +45,11 @@ public class Problem12 {
       final var x = DICTIONARY.get(10 * rank);
 
       switch (digit) {
-        case 1:
-          roman.push(i);
+        case 4:
+          roman.push(i + v);
           break;
         case 5:
           roman.push(v);
-          break;
-        case 4:
-          roman.push(i + v);
           break;
         case 9:
           roman.push(i + x);
@@ -75,10 +72,36 @@ public class Problem12 {
 
   private static Stream<Arguments> data() {
     return Stream.of(
+        Arguments.of(1, "I"),
+        Arguments.of(2, "II"),
         Arguments.of(3, "III"),
         Arguments.of(4, "IV"),
-        Arguments.of(58, "LVIII"),
-        Arguments.of(1994, "MCMXCIV")
+        Arguments.of(5, "V"),
+        Arguments.of(6, "VI"),
+        Arguments.of(7, "VII"),
+        Arguments.of(8, "VIII"),
+        Arguments.of(9, "IX"),
+        Arguments.of(10, "X"),
+        Arguments.of(20, "XX"),
+        Arguments.of(30, "XXX"),
+        Arguments.of(40, "XL"),
+        Arguments.of(50, "L"),
+        Arguments.of(60, "LX"),
+        Arguments.of(70, "LXX"),
+        Arguments.of(80, "LXXX"),
+        Arguments.of(90, "XC"),
+        Arguments.of(100, "C"),
+        Arguments.of(200, "CC"),
+        Arguments.of(300, "CCC"),
+        Arguments.of(400, "CD"),
+        Arguments.of(500, "D"),
+        Arguments.of(600, "DC"),
+        Arguments.of(700, "DCC"),
+        Arguments.of(800, "DCCC"),
+        Arguments.of(900, "CM"),
+        Arguments.of(1000, "M"),
+        Arguments.of(2000, "MM"),
+        Arguments.of(3000, "MMM")
     );
   }
 }
