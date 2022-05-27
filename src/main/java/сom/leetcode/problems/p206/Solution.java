b97e53e03dae9e43fed47f0a1a2213fb74a6011c
Pavel Padalka <ppadalka.pw@gmail.com>
2022-05-27 19:13:10 +0300

    Add iterative solution for 206th promblem

---
+++ src/main/java/сom/leetcode/problems/p206/Solution.java
@@ -0,0 +1,8 @@
+package сom.leetcode.problems.p206;
+
+/**
+ * <a href="https://leetcode.com/problems/reverse-linked-list/">206. Reverse Linked List</a>
+ */
+public interface Solution {
+  ListNode reverseList(ListNode head);
+}
---
+++ src/test/java/сom/leetcode/problems/p206/SolutionTest.java
@@ -0,0 +1,68 @@
+package сom.leetcode.problems.p206;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.notNullValue;
+
+import java.util.stream.Stream;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+@DisplayName("206. Reverse Linked List")
+public class SolutionTest {
+
+  @ParameterizedTest
+  @MethodSource("data")
+  void testIterative(ListNode input, ListNode output) {
+    test(new Iterative(), input, output);
+  }
+
+  @ParameterizedTest
+  @MethodSource("data")
+  void testRecursive(ListNode input, ListNode output) {
+    test(new Recursive(), input, output);
+  }
+
+  void test(Solution solution, ListNode input, ListNode output) {
+    // when
+    var result = solution.reverseList(input);
+    // then
+    for (; output != null; output = output.next, result = result.next) {
+      assertThat(result, notNullValue());
+      assertThat(result.val, is(output.val));
+    }
+  }
+
+  static Stream<Arguments> data() {
+    return Stream.of(
+        Arguments.of(
+            null,
+            null
+        ),
+        Arguments.of(
+            listOf(new int[] {1}),
+            listOf(new int[] {1})
+        ),
+        Arguments.of(
+            listOf(new int[] {1, 2, 3, 4, 5}),
+            listOf(new int[] {5, 4, 3, 2, 1})
+        )
+    );
+  }
+
+  static ListNode listOf(int[] arr) {
+    ListNode prev, head;
+
+    head = prev = new ListNode(arr[0]);
+
+    for (int i = 1; i < arr.length; i++) {
+      prev.next = new ListNode(arr[i]);
+      prev = prev.next;
+    }
+
+    return head;
+  }
+}
