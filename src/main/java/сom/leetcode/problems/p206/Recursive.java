1ac74d63fc5f12057b9641f644459ef8f06d5606
Pavel Padalka <ppadalka.pw@gmail.com>
2022-05-28 23:02:05 +0300

    Add recursive solution for 206th problem

--- src/main/java/сom/leetcode/problems/p206/Recursive.java
+++ src/main/java/сom/leetcode/problems/p206/Recursive.java
@@ -1,9 +1,24 @@
 package сom.leetcode.problems.p206;
 
+import сom.leetcode.annotations.MySolution;
+
+@MySolution
 public class Recursive implements Solution {
 
   @Override
   public ListNode reverseList(ListNode head) {
-    return head;
+    if ((head == null) || (head.next == null)) {
+      return head;
+    }
+
+    return reverse(null, head, head.next);
+  }
+
+  private ListNode reverse(ListNode prev, ListNode curr, ListNode next) {
+    curr.next = prev;
+
+    return (next == null)
+        ? curr
+        : reverse(curr, next, next.next);
   }
 }
